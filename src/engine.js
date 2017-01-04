1a204b89f519af8f5d122dab43da8575b7fe3738
Chris Hitchcott <hitchcott@gmail.com>
2017-01-04 16:48:55 +0000

    update deps (esp. provider engine), minor refactor

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,19 @@
 import fs from 'fs';
 import Wallet from 'ethereumjs-wallet';
 import ProviderEngine from 'web3-provider-engine';
+import DefaultFixture from 'web3-provider-engine/subproviders/default-fixture';
+import NonceTrackerSubprovider from 'web3-provider-engine/subproviders/nonce-tracker';
+import SanitizingSubprovider from 'web3-provider-engine/subproviders/sanitizer';
+import CacheSubprovider from 'web3-provider-engine/subproviders/cache';
+import FilterSubprovider from 'web3-provider-engine/subproviders/filters';
+import RpcSubprovider from 'web3-provider-engine/subproviders/rpc';
 
 import SigmateSubprovider from './subprovider';
 
 import { DEFAULT_PATH } from './constants';
 
-export default function ({ path = DEFAULT_PATH, label, password }) {
+export default function ({ keystore, providerUrl }) {
+  const { path = DEFAULT_PATH, label, password } = keystore;
   const dest = `${path}/${label}`;
   if (!path) { throw new Error('Password not provided'); }
   if (!label) { throw new Error('Keystore name not provided'); }
@@ -22,7 +29,22 @@ export default function ({ path = DEFAULT_PATH, label, password }) {
   }).filter(w => w);
 
   const providerEngine = new ProviderEngine();
+  providerEngine.addProvider(new DefaultFixture());
+  providerEngine.addProvider(new NonceTrackerSubprovider());
+  providerEngine.addProvider(new SanitizingSubprovider());
+  providerEngine.addProvider(new CacheSubprovider());
+  providerEngine.addProvider(new FilterSubprovider());
   providerEngine.addProvider(new SigmateSubprovider(wallets));
+  const rpcSubprovider = new RpcSubprovider({ rpcUrl: providerUrl });
+  // TODO additional logging
+  // const oldHandler = rpcSubprovider.handleRequest;
+  // rpcSubprovider.handleRequest = (...args) => {
+  //   console.log('handling', args);
+  //   return oldHandler.apply(rpcSubprovider, args);
+  // }
+  // providerEngine.on('error', err => console.log('err', err.stack));
+  providerEngine.addProvider(rpcSubprovider);
+  providerEngine.start();
   return {
     providerEngine,
     coinbase: wallets[0].getAddressString(),
