35a9d3ccad691249328aeafa1a65604cb2e4d21b
Chris Hitchcott <hitchcott@gmail.com>
2017-01-12 12:59:38 +0000

    [refactor] remove unecessary providers

--- src/engine.js
+++ src/engine.js
@@ -1,28 +1,16 @@
 import fs from 'fs';
 import Wallet from 'ethereumjs-wallet';
 import ProviderEngine from 'web3-provider-engine';
-import DefaultFixture from 'web3-provider-engine/subproviders/default-fixture';
 import NonceTrackerSubprovider from 'web3-provider-engine/subproviders/nonce-tracker';
-import SanitizingSubprovider from 'web3-provider-engine/subproviders/sanitizer';
-import CacheSubprovider from 'web3-provider-engine/subproviders/cache';
-import FilterSubprovider from 'web3-provider-engine/subproviders/filters';
 import RpcSubprovider from 'web3-provider-engine/subproviders/rpc';
-import TestRPC from 'ethereumjs-testrpc';
 
-import SigmateSubprovider from './subprovider';
+import WalletSubprovider from './walletProvider';
 
 import { DEFAULT_PATH } from './constants';
 
 export default function ({ keystore, rpcUrl, virtual, prefund }) {
 
   const providerEngine = new ProviderEngine();
-
-  // defaults
-  providerEngine.addProvider(new DefaultFixture());
-  providerEngine.addProvider(new NonceTrackerSubprovider());
-  providerEngine.addProvider(new SanitizingSubprovider());
-  providerEngine.addProvider(new CacheSubprovider());
-  providerEngine.addProvider(new FilterSubprovider());
 
   let wallets;
 
@@ -40,31 +28,19 @@ export default function ({ keystore, rpcUrl, virtual, prefund }) {
         return null;
       }
     }).filter(w => w);
-    providerEngine.addProvider(new SigmateSubprovider(wallets));
-  }
-
-  if (rpcUrl && virtual) { throw new Error('Specify `rpcUrl` or `virtual` - not both'); }
-
-  if (virtual) {
-    console.log('adding Testrpc provider....');
-    providerEngine.addProvider(TestRPC.provider());
+    providerEngine.addProvider(new WalletSubprovider(wallets));
   }
 
   if (rpcUrl) {
     const rpcSubprovider = new RpcSubprovider({ rpcUrl });
-    // TODO additional logging
-    // const oldHandler = rpcSubprovider.handleRequest;
-    // rpcSubprovider.handleRequest = (...args) => {
-    //   console.log('handling', args);
-    //   return oldHandler.apply(rpcSubprovider, args);
-    // }
-    // providerEngine.on('error', err => console.log('err', err.stack));
     providerEngine.addProvider(rpcSubprovider);
   }
 
-  providerEngine.start();
+  // defaults
+  providerEngine.addProvider(new NonceTrackerSubprovider());
+  // init
+  providerEngine._fetchLatestBlock();
 
-  // TODO prefund!
   return {
     providerEngine,
     coinbase: wallets && wallets[0].getAddressString(),
